971d2b48449f4e21edbcf5706eca087ad6006b60
smontoya <smontoya@leaf.cl>
2012-11-14 22:47:09 -0300

    Creacion de las primeras clases

---
+++ src/java/Admin.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sam
+ */
+public class Admin extends Usuario{
+    
+}
---
+++ src/java/Usuario.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sam
+ */
+public class Usuario {
+    private String nombre;
+    private String appellidos;
+    
+}
